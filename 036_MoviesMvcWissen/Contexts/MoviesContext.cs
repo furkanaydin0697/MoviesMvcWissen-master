95add74e8ce66e0e84b7d34402c1d6b0389ee1c5
Section2 <Section2@DESKTOP-KHOPMPR>
2020-02-18 21:10:19 +0300

    18.02.2020

--- 036_MoviesMvcWissen/Contexts/MoviesContext.cs
+++ 036_MoviesMvcWissen/Contexts/MoviesContext.cs
@@ -12,7 +12,9 @@ namespace _036_MoviesMvcWissen.Contexts
     {
         public MoviesContext() : base("MoviesContext")
         {
-            
+            //codefirst yapısında kullanmak önemli
+            //Disable initilazer
+            Database.SetInitializer<MoviesContext>(null);
         }
 
         public virtual DbSet<Movie> Movies { get; set; }
@@ -20,6 +22,9 @@ namespace _036_MoviesMvcWissen.Contexts
         public virtual DbSet<Review> Reviews { get; set; }
         public virtual DbSet<MovieDirector> MovieDirectors { get; set; }
         public virtual DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public virtual DbSet<vwUsers> vwUsers { get; set; }
+
 
     }
 }
